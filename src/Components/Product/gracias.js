9ad190313acbb615d14d869af31403c4dba75904
carlosmx45 <carlosjavierfarias@hotmail.com>
2022-07-25 08:10:50 -0500

    fuck off

--- src/Components/Product/gracias.js
+++ src/Components/Product/gracias.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Button, Flex, Center, Heading, Input, Spacer, Stack, Text, Tag, SimpleGrid, GridItem, Image, Spinner } from "@chakra-ui/react";
+import cartaFuegoAzul3 from '../cards/cartaFuegoAzul3.png';
 
 function Gracias(props) {
     return (
@@ -10,7 +11,10 @@ function Gracias(props) {
                 <Image src="https://i.pinimg.com/originals/8c/87/48/8c874897fee0a13abab123012e9345fb.gif" h="sm" />
             </Center>
             <Heading noOfLines={2} size="sm" fontWeight="normal">
+            <Button><a href='/' download={cartaFuegoAzul3} className="Text">Descargar</a></Button>
+            <Heading mt={2} noOfLines={2} size="sm" fontWeight="normal">
                 Ya Terminaste por Aqui.
+            </Heading>
             </Heading>
             <Button><a href="/" className="Text">Regresar a Home</a></Button>
         </Box>
